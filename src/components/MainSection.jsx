6125e1ef80f8d1d53ab245a2d46d0a16eaf7a73a
MewKay <miorakanto25z@gmail.com>
2024-09-16 13:50:20 +0300

    Create main container

---
+++ src/components/MainSection.jsx
@@ -0,0 +1,10 @@
+function MainSection() {
+  return (
+    <main>
+      <div className="edit-section"></div>
+      <div className="display-section"></div>
+    </main>
+  );
+}
+
+export default MainSection;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./components/Header";
+import MainSection from "./components/MainSection";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
+    <MainSection />
   </StrictMode>
 );
