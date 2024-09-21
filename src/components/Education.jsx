4d85fcfa17e3cac76bb57fd1024d401fdca23e58
MewKay <miorakanto25z@gmail.com>
2024-09-21 23:54:32 +0300

    Modify Education section to select inputs

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,6 +3,34 @@ function Education() {
   const schoolNameInputId = "school-name-input";
   const studyTitleInputId = "study-title-input";
   const endDateInputId = "end-date-input";
+  const endDateMonthInputId = "end-date-month-input";
+  const endDateYearInputId = "end-date-year-input";
+  const endDateSelectClass = "end-date-select";
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const years = (() => {
+    const centuryList = [];
+    let todayYear = new Date().getFullYear();
+
+    for (let i = 0; i < 100; i += 1, todayYear -= 1) {
+      centuryList.push(todayYear);
+    }
+
+    return centuryList;
+  })();
 
   return (
     <section id="education-section">
@@ -20,7 +48,41 @@ function Education() {
 
       <div className={inputClassName}>
         <label htmlFor={endDateInputId}>Completion By :</label>
-        <input id={endDateInputId} type="text" required></input>
+        <div id={endDateInputId}>
+          <span className={endDateSelectClass}>
+            <label htmlFor={endDateMonthInputId}>Month :</label>
+            <select
+              id={endDateMonthInputId}
+              name="completionMonth"
+              defaultValue={months[0]}
+            >
+              {months.map((month) => {
+                return (
+                  <option value={month} key={crypto.randomUUID()}>
+                    {month}
+                  </option>
+                );
+              })}
+            </select>
+          </span>
+
+          <span className={endDateSelectClass}>
+            <label htmlFor={endDateYearInputId}>Year :</label>
+            <select
+              id={endDateYearInputId}
+              name="completionYear"
+              defaultValue={years[0]}
+            >
+              {years.map((year) => {
+                return (
+                  <option value={year} key={crypto.randomUUID()}>
+                    {year}
+                  </option>
+                );
+              })}
+            </select>
+          </span>
+        </div>
       </div>
     </section>
   );
