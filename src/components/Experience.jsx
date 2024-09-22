b3f33be070da6db410f3f99ab3d2baff13a2346c
MewKay <miorakanto25z@gmail.com>
2024-09-22 22:32:42 +0300

    Add work duration selectors for Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,10 @@
+import MonthSelector from "./MonthSelector";
+
 function Experience() {
+  const workDurationSelectClass = "work-duration-select-class";
+  const workStartNamePartId = "work-start";
+  const workEndNamePartId = "work-end";
+
   return (
     <section>
       <h2>Add Experience</h2>
@@ -11,6 +17,26 @@ function Experience() {
       <div>
         <label>Position Title :</label>
         <input type="text" required></input>
+      </div>
+
+      <div>
+        <label>Work Duration :</label>
+
+        <div id="work-start-select">
+          <label>From :</label>
+          <MonthSelector
+            className={workDurationSelectClass}
+            namePartId={workStartNamePartId}
+          />
+        </div>
+
+        <div id="work-end-select">
+          <label>To :</label>
+          <MonthSelector
+            className={workDurationSelectClass}
+            namePartId={workEndNamePartId}
+          />
+        </div>
       </div>
     </section>
   );
