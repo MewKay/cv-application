53a2bd06f0c60cd00bb85ba651cdde313e2b14fe
MewKay <miorakanto25z@gmail.com>
2024-09-23 13:22:13 +0300

    Create mock resume data

---
+++ src/resumeData.js
@@ -0,0 +1,37 @@
+const resumeData = {
+  generalInfo: {
+    fullName: "John Jones",
+    phoneNumber: "111-222-3333",
+    email: "john@worker.net",
+  },
+  education: [
+    {
+      schoolName: "Mediun College",
+      studyTitle: "Bachelors of Science in Computer Science",
+      endDate: "August 2020",
+    },
+    {
+      schoolName: "High University",
+      studyTitle: "Master of Science in Cybersecurity",
+      endDate: "August 2022",
+    },
+  ],
+  experience: [
+    {
+      companyName: "Mandor Tech",
+      positionTitle: "Full Stack Developer",
+      workStart: "December 2022",
+      workEnd: "July 2024",
+      mainResp: [
+        "Developed and maintained scalable web applications using JavaScript, React, and Node.js, improving platform efficiency by 15%.",
+        "Collaborated with cross-functional teams including product managers and designers to deliver high-quality features on tight deadlines.",
+        "Implemented API integrations and optimized database queries, reducing response times by 20%.",
+        "Conducted code reviews, provided mentorship to junior developers, and introduced best practices for version control and testing.",
+        "Troubleshot and resolved software bugs, ensuring a seamless user experience and reducing critical issues by 30%.",
+        "Led the migration of legacy code to a modern microservices architecture, resulting in improved system scalability.",
+      ],
+    },
+  ],
+};
+
+export default resumeData;
