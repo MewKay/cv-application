6f28329ac51e037f8b0e07e4b009248be8875174
MewKay <miorakanto25z@gmail.com>
2024-09-23 11:12:05 +0300

    Rearrange Education component's inputs to their own component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,32 @@
 import MonthSelector from "./MonthSelector";
 
+function SchoolNameInput({ className, inputId }) {
+  return (
+    <div className={className}>
+      <label htmlFor={inputId}>School Name :</label>
+      <input id={inputId} type="text" required></input>
+    </div>
+  );
+}
+
+function StudyTitleInput({ className, inputId }) {
+  return (
+    <div className={className}>
+      <label htmlFor={inputId}>Title of Study :</label>
+      <input id={inputId} type="text" required></input>
+    </div>
+  );
+}
+
+function EducationEndDateInput({ className, inputId, namePartId }) {
+  return (
+    <fieldset className={className}>
+      <legend>Completion By :</legend>
+      <MonthSelector id={inputId} namePartId={namePartId} />
+    </fieldset>
+  );
+}
+
 function Education() {
   const inputClassName = "education-input";
   const schoolNameInputId = "school-name-input";
@@ -10,21 +37,13 @@ function Education() {
   return (
     <section id="education-section">
       <h2>Add Education</h2>
-
-      <div className={inputClassName}>
-        <label htmlFor={schoolNameInputId}>School Name :</label>
-        <input id={schoolNameInputId} type="text" required></input>
-      </div>
-
-      <div className={inputClassName}>
-        <label htmlFor={studyTitleInputId}>Title of Study :</label>
-        <input id={studyTitleInputId} type="text" required></input>
-      </div>
-
-      <fieldset className={inputClassName}>
-        <legend>Completion By :</legend>
-        <MonthSelector id={educationEndDateInputId} namePartId={namePartId} />
-      </fieldset>
+      <SchoolNameInput className={inputClassName} inputId={schoolNameInputId} />
+      <StudyTitleInput className={inputClassName} inputId={studyTitleInputId} />
+      <EducationEndDateInput
+        className={inputClassName}
+        inputId={educationEndDateInputId}
+        namePartId={namePartId}
+      />
     </section>
   );
 }
