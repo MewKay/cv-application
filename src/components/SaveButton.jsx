b96d53a80e8820591141d4ae2d4e8a1ddf1982ab
MewKay <miorakanto25z@gmail.com>
2024-09-23 12:10:06 +0300

    Create SaveButton

---
+++ src/components/SaveButton.jsx
@@ -0,0 +1,9 @@
+function SaveButton({ className, handleClick }) {
+  return (
+    <button className={className} onClick={handleClick}>
+      Save
+    </button>
+  );
+}
+
+export default SaveButton;
